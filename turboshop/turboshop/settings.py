de7aa0faf980b2e9d9d61433e99e12a9dd218d82
Aleks-Kas <spawn_kas@mail.ru>
2023-03-14 17:40:26 +0300

    0.003 Создание представления каталога. ToDo - отредактировать Static

--- turboshop/turboshop/settings.py
+++ turboshop/turboshop/settings.py
@@ -63,7 +63,10 @@ ROOT_URLCONF = 'turboshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
